d06807c2a85bfe98bd2cd0158d4bf788eab73646
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-21 18:07:22 +0500

    code is added

--- script.js
+++ script.js
@@ -10,5 +10,32 @@ const questions = [
             { text: "Germany", correct: false},
             { text: "Argentina", correct: false}
         ]
+    },
+    {
+        question: "Which European country is home to the famous Alpine Mountains?",
+        answers: [
+            { text: "Switzerland", correct: true},
+            { text: "France", correct: false},
+            { text: "Germany", correct: false},
+            { text: "United Kingdom", correct: false}
+        ]
+    },
+    {
+        question: "Who wrote the novel 'The Great Gatsby'?",
+        answers: [
+            { text: "F. Scott Fitzgerald", correct: true},
+            { text: "J.K. Rowling", correct: false},
+            { text: "Ernest Hemingway", correct: false},
+            { text: "Harper Lee", correct: false}
+        ]
+    },
+    {
+        question: "Which famous American author wrote the classic novel 'To Kill a Mockingbird'?",
+        answers: [
+            { text: "Harper Lee", correct: true},
+            { text: "George Orwell", correct: false},
+            { text: "F. Scott Fitzgerald", correct: false},
+            { text: "J.K. Rowling", correct: false}
+        ]
     }
 ]
