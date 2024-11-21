b89bc06889b12698ef2d4b2bb4063b961174b479
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-21 18:05:10 +0500

    code is added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const questions = [
+    {
+        question: "Who won the 2020 FIFA World Cup?",
+        // options: ["Brazil", "Spain", "Germany", "Argentina"],
+        // answer: "Brazil"
+        answers: [
+            { text: "Brazil", correct: true},
+            { text: "Spain", correct: false},
+            { text: "Germany", correct: false},
+            { text: "Argentina", correct: false}
+        ]
+    }
+]
