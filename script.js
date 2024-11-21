08b520c0da62ba56e3067734883762678ae1cb21
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-21 18:10:30 +0500

    code is added

--- script.js
+++ script.js
@@ -41,3 +41,5 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
